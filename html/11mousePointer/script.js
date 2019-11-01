dbf37277b19f76e4f8a68b17c82b1bdd3eefc053
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-11-01 14:49:17 +0100

    Arrow Opdracht Af

--- html/11mousePointer/script.js
+++ html/11mousePointer/script.js
@@ -7,10 +7,18 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let mousePos = new Vector2d(0, 0);
-let difference = new Vector2d(0, 0);
-let A = new Arrow(new Vector2d((width/2), (height/2)), new Vector2d(80, 50),
-                  new Vector2d(20, 40), "#000040", "#800000", 270);
+let A = new Arrow(new Vector2d((width/5 * 1), (height/5 * 1)),
+ new Vector2d(180, 50), new Vector2d(10, 20),
+ "#00F4FF", "#FFFFFF", "#0000FF", "#00F4FF", 270, true);
+let B = new Arrow(new Vector2d((width/5 * 4), (height/5 * 1)),
+ new Vector2d(180, 50), new Vector2d(10, 20),
+ "#A6FF00", "#FFFFFF", "#00FF00", "#A6FF00", 270, true);
+let C = new Arrow(new Vector2d((width/5 * 1), (height/5 * 4)),
+ new Vector2d(180, 50), new Vector2d(10, 20),
+ "#FF0000", "#FFFFFF", "#960000", "#FF0000", 270, true);
+let D = new Arrow(new Vector2d((width/5 * 4), (height/5 * 4)),
+ new Vector2d(180, 50), new Vector2d(10, 20),
+ "#800060", "#FFFFFF", "#530080", "#800060", 270, true);
 
 function animate(){
 
@@ -18,25 +26,20 @@ function animate(){
    context.clearRect(0, 0, width, height);
 
    A.draw(context);
+   B.draw(context);
+   C.draw(context);
+   D.draw(context);
 }
 
 animate();
 
-window.addEventListener('click', (evt)=>{
-   mousePos.dx = evt.clientX;
-   mousePos.dy = evt.clientY;
-   difference.dx = mousePos.dx - A.position.dx;
-   difference.dy = mousePos.dy - A.position.dy;
-
-   /*if (difference.dx > 0){
-
-   }
-   else if (difference.dx < 0){
-
-   }
-   else {
-
-   }*/
-
-   console.log(difference);
-});
+function getRandomColor()
+ {
+     var color = "";
+     for(var i = 0; i < 3; i++) {
+         //Choose a random number and convert it to hexadecimal
+         var sub = Math.floor(Math.random() * 256).toString(16);
+         color += (sub.length == 1 ? "0" + sub : sub);
+     }
+     return "#" + color;
+ }
