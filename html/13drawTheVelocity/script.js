f2d382cb226bb7cfba61d64ef4d9e2fe4ffe246d
Wayne Hofstra <wayne.hofstra@hva.nl>
2019-11-26 15:01:48 +0100

    13drawTheVelocity without DPoint-array

--- html/13drawTheVelocity/script.js
+++ html/13drawTheVelocity/script.js
@@ -8,24 +8,57 @@ canvas.width = width;
 canvas.height = height;
 
 let A = new Arrow(new Vector2d(0, 0), new Vector2d(65, 40), new Vector2d(10, 20),
- getRandomColor(), getRandomColor(), getRandomColor(), getRandomColor(), 270, false);
-let B = new DPoint(new Vector2d(canvas.width / 2, canvas.height / 2), 20,
- getRandomColor(), getRandomColor(), new Vector2d(100, 100), new Vector2d(0, 0));
+  getRandomColor(), getRandomColor(), getRandomColor(), getRandomColor(), 270);
+let B = new DPoint(new Vector2d(getRandom(canvas.width), getRandom(canvas.height)), 20,
+  getRandomColor(), getRandomColor(), new Vector2d(220, getRandomMin(30, 400)), 10, true);
+let C = new Arrow(new Vector2d(0, 0), new Vector2d(65, 40), new Vector2d(10, 20),
+  getRandomColor(), getRandomColor(), getRandomColor(), getRandomColor(), 270);
+let D = new DPoint(new Vector2d(getRandom(canvas.width), getRandom(canvas.height)), 20,
+  getRandomColor(), getRandomColor(), new Vector2d(220, getRandomMin(30, 400)), 10, true);
+let E = new Arrow(new Vector2d(0, 0), new Vector2d(65, 40), new Vector2d(10, 20),
+  getRandomColor(), getRandomColor(), getRandomColor(), getRandomColor(), 270);
+let F = new DPoint(new Vector2d(getRandom(canvas.width), getRandom(canvas.height)), 20,
+   getRandomColor(), getRandomColor(), new Vector2d(220, getRandomMin(30, 400)), 10, true);
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  A.position = B.position;
-  A.angle = Math.atan(B.velocity.dy / B.velocity.dx);
-  A.draw(context);
+  B.bounce(new Vector2d(canvas.width, canvas.height));
+  D.bounce(new Vector2d(canvas.width, canvas.height));
+  F.bounce(new Vector2d(canvas.width, canvas.height));
 
   B.update();
+  D.update();
+  F.update();
+  A.position = B.position;
+  C.position = D.position;
+  E.position = F.position;
+  A.checkBall(B);
+  C.checkBall(D);
+  E.checkBall(F);
+
+  A.shaftLength = (B.velocity.magnitude / 4);
+  C.shaftLength = (D.velocity.magnitude / 4);
+  E.shaftLength = (F.velocity.magnitude / 4);
+
+  A.draw(context);
   B.draw(context);
-  B.bounce(new Vector2d(canvas.width, canvas.height));
+  C.draw(context);
+  D.draw(context);
+  E.draw(context);
+  F.draw(context);
 }
 
 animate();
+
+function getRandom(max){
+  return Math.floor(Math.random()*max);
+}
+
+function getRandomMin(min, max){
+  return Math.floor(Math.random()*(max - min) + min);
+}
 
 function getRandomColor()
 {
